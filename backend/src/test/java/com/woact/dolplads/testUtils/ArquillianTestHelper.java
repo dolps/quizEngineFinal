4d8c9e1f734a8f5c22ca4d18edd47508820ca7c0
dolps <dolplads@gmail.com>
2016-10-16 22:12:05 +0200

    hm

--- backend/src/test/java/com/woact/dolplads/testUtils/ArquillianTestHelper.java
+++ backend/src/test/java/com/woact/dolplads/testUtils/ArquillianTestHelper.java
@@ -3,6 +3,7 @@ package com.woact.dolplads.testUtils;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.runner.RunWith;
 
@@ -16,7 +17,8 @@ public abstract class ArquillianTestHelper {
         JavaArchive war = ShrinkWrap.create(JavaArchive.class)
                 .addPackages(true, "com.woact.dolplads")
                 .addPackages(true, "org.apache.commons.codec")
-                .addAsResource("META-INF/persistence.xml");
+                .addAsResource("META-INF/persistence.xml")
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
 
         System.out.println(war.toString(true));
         return war;
